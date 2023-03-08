e5614ed50d77832cbd06fa9884bbc46df4d4a478
regmi2 <regmipallav@gmail.com>
2023-03-08 12:54:12 -0600

    updated features so it takes in props from the WhatGPT3.jsx calling of <Feature />, this way each feature can have specific text based on its location - finalized CSS as well for the whole section

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -6,7 +6,12 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature 
+          title="What is GPT-3" 
+          text="GPT-3 (Generative Pre-trained Transformer 3) is a neural network machine learning model. 
+          It consumed vast swaths of internet data to understand how to generate all types of text. A small input
+          text as a prompt can return a large volume of relevant and sophisticated machine-generated text
+          for your personal or business use. It was developed by a US company called OpenAI." />
       </div>
 
       <div className='gpt3__whatgpt3-heading'>
@@ -17,9 +22,23 @@ const WhatGPT3 = () => {
       </div>
 
       <div className='gpt3__whatgpt3-container'>
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature 
+          title="Chatbots"
+          text="Anyone can use the chatbot by signing up for an account on chat.openai.com.
+          From there, type a prompt to get started. "
+        />
+
+        <Feature
+          title="Knowledgebase"
+          text="Using data from the internet, GPT-3 can bring together disparate and relevant
+          information in a matter of seconds with a sufficiently specific prompt.
+          "/>
+
+        <Feature 
+          title="Education"
+          text="GPT-3 has the potential to transform the way we learn and retrieve knowledge.
+          Students around the world are already making the most out of it."
+                />
       </div>
     </div>
   )
