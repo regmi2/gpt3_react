2ceaab715a03a78362d512b0b999783ab6800703
regmi2 <regmipallav@gmail.com>
2023-03-08 13:24:45 -0600

    updated the features section f the webpage, using feature component again to present text in order to maintain aesthetics, used an array and the map function to display the individual features -- css implemented successfully

--- src/containers/Features/Features.jsx
+++ src/containers/Features/Features.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './Features.css'
 
-const Features = () => {
-  return (
-    <div>
-    Features
+const featuresData = [
+  {
+    title: 'Generate Valuable Content Instantly',
+    text: 'How can you leverage GPT-3 to create value for your community? The right question can return priceless value.'
+  },
+  {
+    title: 'Extend your Mind Beyond your Brain',
+    text: 'The chatbot allows you to save your previous chats and follow up at a later time. Reference those fresh ideas without losing the initial context.'
+  },
+  {
+    title: 'Incorporate it Into Your Apps',
+    text: "You can use GPT-3's API to turn your app into a GPT-3 powered search engine. What experiences could you craft through this?."
+  },
+  {
+    title: 'Explore Unique Voices',
+    text: 'In creating your value, you can ask GPT-3 to share information in a certain tone or voice giving you endless room for creative value communication.'
+  }
+]
+
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">The Future is Now.
+          Step into the World of AI & Build Your Future</h1>
+      <p>Request Early Access to Get Started</p>
     </div>
-  )
-}
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature 
+        title={item.title} 
+        text={item.text} 
+        key={item.title + index} 
+        />
+      ))}
+    </div>
+  </div>
+)
+
+
 
 export default Features
