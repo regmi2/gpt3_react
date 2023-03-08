65a6a01cc96ad4766422f474bb1f0a0c67c41d65
regmi2 <regmipallav@gmail.com>
2023-03-08 11:36:14 -0600

    header html and css updated, optimized for mobile as well

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -6,11 +6,11 @@ import ai from'../../assets/ai.png'
 const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
-      <div className='gpt3__header-contnet'>
+
+      <div className='gpt3__header-content'>
         <h1 className='gradient__text'>
           Let's Build Next Gen Tools with GPT-3 by OpenAI
         </h1>
-
         <p className="gpt3__header-description">
           By the end of this decade, Web3, AI, & AR will 
           a significant part of our daily lives. From commerce
@@ -19,7 +19,7 @@ const Header = () => {
           unique value proposition to your life.
         </p>
 
-        <div className='gpt3__haeader-content__input'>
+        <div className='gpt3__header-content__input'>
           <input type="email" placeholder="Your Email Address"/>
           <button type="button"> Get Started </button>
         </div>
@@ -29,13 +29,12 @@ const Header = () => {
           <p> 1600 people requested access in the last 24 hours</p>
         </div>
 
-        <div className='gpt3__header-image'>
-          <img src={ai} alt="ai"/>
-
         </div>
 
+        <div className='gpt3__header-image'>
+          <img src={ai} alt="ai"/>
+        </div>
 
-      </div>
     </div>
   )
 }
