3f5e024f07ee64df8f5265fcc06205451198e14a
regmi2 <regmipallav@gmail.com>
2023-03-08 13:36:35 -0600

    implemnted structure and styling for possibility and CTA, reused styling from previous sections for possibility to save time, aesthetically pleasing and ready for mobile + tabs as well

--- src/containers/Possibility/Possibility.jsx
+++ src/containers/Possibility/Possibility.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 
 const Possibility = () => {
   return (
-    <div>
-    Possibility
+    <div className='gpt3__possibility section__padding' id='possibility'>
+
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="VR wearing woman exploring possibilities" />
+      </div>
+
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'> The possibilities are Beyond Our Imagination </h1>
+        <p> In the coming future, we could see GPT-3 implemented with VR and AR to launch us into a new era of tech possibilities.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   )
 }
