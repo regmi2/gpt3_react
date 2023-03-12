bebb51d979845056c1738b335b1536378f11a825
regmi2 <regmipallav@gmail.com>
2023-03-12 14:18:46 -0500

    footer finalized -- next is making text and links work

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
+import gpt3Logo from '../../assets/logo.svg'
 import './Footer.css'
 
-const Footer = () => {
-  return (
-    <div>
-    Footer
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
-  )
-}
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p> Anuv LLC (c) 2023 <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Anuv LLC</p>
+        <p>(555)555-5555</p>
+        <p>info@anuv.io</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2023 Anuv. All rights reserved.</p>
+    </div>
+  </div>
+);
 
 export default Footer
